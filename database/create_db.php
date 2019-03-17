edc54392632f145431ca40c78afa704c3a6f01ee
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2019-03-17 09:50:58 +0530

    Created db and table

---
+++ database/create_db.php
@@ -0,0 +1,69 @@
+<?php
+    $servername="localhost";  
+    $username="root";
+    $password="";
+    
+    
+    try{
+    
+    $sql_db="CREATE DATABASE User_info";
+    $conn=new PDO("mysql:host=$servername",$username,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $conn->exec($sql_db);
+    echo"Database created sucesfully";
+    }
+    
+    catch(PDOException $e){
+        echo"Database creation failed<br/>".$e->getmessage();
+    }
+    
+    
+    $conn=null;
+    
+    
+    
+    
+    
+    
+    $dbname="User_info";
+    $dsn="mysql:host=$servername;dbname=$dbname";
+    
+    
+    try{    
+            $conn=new PDO($dsn,$username,$password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            $sql_query="CREATE TABLE user_details(
+                                                    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                                                    first_name VARCHAR(30) NOT NULL,
+                                                    last_name VARCHAR(30) NOT NULL,
+                                                    email VARCHAR(50),
+                                                    reg_Date TIMESTAMP,
+                                                    mobile_n0 INT(10) UNSIGNED
+                                                 )";
+            $conn->exec($sql_query);
+            echo"<br/>Table create sucessfully";                                     
+
+
+        }        
+    catch( PDOException $e)            
+        {
+            echo"Connection failed<br/>".$e->getMessage();
+        }    
+        
+        $conn=null;
+    ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ header.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title><?php echo $title ?></title>
+        <style>
+            body{
+                background-color:linen;
+            }
+        </style>
+    </head>
+
+    <body>
+        <img src="./images/logo.jpg" width=100 height=100>
+    </body>
+    </html>
+
+        
