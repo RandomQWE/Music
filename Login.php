62b58419add0e3cad82d2d9416b331bb5f140138
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2019-03-18 20:25:56 +0530

    Validation login.php

--- Login.php
+++ Login.php
@@ -1,6 +1,50 @@
 <?php
     $title="login";
     include 'header.php'
+    
+    
+    session_start();
+    if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"]==true){
+            header("location:welcome.php");
+            exit;
+    
+        $servername="localhost";
+        $username="root";
+        $password="";
+        $dbname="User_info";    
+        $dsn="mysql:host=$servername;dbname=$dbname";
+        
+        $emailErr= $email= $password= $passwordErr= "";
+        
+        
+        
+        $con=new PDO($dsn,$username,$password);
+
+        function test_input($data)
+        {
+            $data=trim($data);
+            $data=stripslashes($data);
+            $data=htmlspecialchars($data);
+        }
+
+        if ($_SERVER["REQUEST_METHOD"]=="POST"){
+
+            if (empty($_POST["email"]))
+                $emailErr="Email is required";
+            else
+            {
+                $email=test_input($_POST["email"]){
+                    if(!filter_var($email,FILTER_VALIDATE_EMAIL))
+                        {
+                            $emailErr="Invalid format";
+                        }
+                }
+            }
+        }
+
+
+
+
     ?>
 <!DOCTYPE html>
 <html>
@@ -11,24 +55,71 @@
         <style>
            fieldset.logon_font{
                
-               border-radius: 30px;
-               border-style:solid;
+                border-radius: 30px;
+                border-style:solid;
+                width:800px;
             }
             .b{
             background-image:url(./images/music.jpg);
-            height: 100%; 
+            height:100%;
+            
+            
+            
             background-size:cover;
-        }
+            }   
             body{
                 font-family: Arial, Helvetica, sans-serif;
 
             }
-        
-        
-        
-        
-        
-        
+            * {
+            box-sizing: border-box;
+            }
+
+           input[type=email]{           
+            width: 500px;
+            padding: 12px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            resize: vertical;
+            margin-left:50px;
+            
+            }
+            
+            
+            label {
+            padding: 12px 12px 12px 0;
+            display: inline-block;
+            }
+            
+            
+            input[type=submit]:hover {
+            background-color: #45a049;
+            }
+            
+            
+            input[type=submit] {
+            margin-left:300px;
+            margin-top:20px;
+            background-color: #4CAF50;
+            color: white;
+            padding: 12px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            
+            }
+            
+            input[type=password]{
+            width: 500px;
+            padding: 12px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            resize: vertical;
+            margin-left:50px;
+
+            
+            }
+
         </style>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -36,11 +127,11 @@
 
     <body>
         <div class="b">
-            <form action="logon_save.php" method="POST">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?> method="POST">
             <fieldset class="logon_font">
-                <div>
+                <div class="a">
                     <label for="username">Username</label>
-                    <input name="username" id="username" type="email/text" placeholder="Enter username or email Address" required >
+                    <input name="username" id="username" type="email" placeholder="Enter username or email Address" required >
                 </div>
                 <div>
                 <label for="usr_pass">Password</label>
--- contactus.php
+++ contactus.php
@@ -1,25 +1,97 @@
-<?php 
-    $title="Contact Us";
-    include 'header.php';
-    $name="";
-    $nameErr="";
+<?php
+$title="Contact Us";
+include'header.php'
 ?>
-<!DOCTYPE html>
+
+
+
+<!DOCTYPE HTML>  
 <html>
-    <head>
-    </head>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
+<?php
+$nameErr = $emailErr  = $websiteErr = "";
+$name = $email  = $comment = $website = "";
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+}
+
+ if ($_SERVER["REQUEST_METHOD"] == "POST") {
+     if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+     } else {
+    $name = test_input($_POST["name"]);
+        if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+      $websiteErr = "Invalid URL"; 
+    }
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  
+}
 
 
-    <body>
-        <form action="customer_help.php" method="POST">
-            <fieldset>
-                <legend>Contact us</legend>
-                    <div>
-                        <label for="contact_name">Name</label>
-                        <input name="contact_name" id="contact_name" type="text" value="<?php echo $name;?>">
-                        <span class="error"> <?php echo $nameErr;?>
-                        
-                    </div>
-            </fieldset>
-        </form>
-    </body>
+
+?>
+
+<h2>ContactUS</h2>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website" value="<?php echo $website;?>">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <br><br>
+
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $comment;
+echo "<br>";
+
+?>
+
+</body>
+</html>
